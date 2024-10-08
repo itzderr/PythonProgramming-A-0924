47c482ea7f9fbaca2271ba6d258e75f10323d028
Derrick Park <derrick8689@gmail.com>
2024-10-08 13:50:14 -0700

    Add matplotlib_examples.py

---
+++ matplotlib_examples.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+# Data
+x = [1, 2, 3, 4, 5]
+y = [1, 4, 9, 16, 25]
+
+# Plotting
+plt.plot(x, y)
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title('Simple Line Plot')
+plt.show()
+
